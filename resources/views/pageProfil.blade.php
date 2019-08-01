19fa4a9969c1ab92943a416fac1b43c8b9c2e615
Unknown <guillaume.frech@gmail.com>
2019-08-01 15:37:32 +0200

    CSS Bouton supprimer / utiliser

--- resources/views/pageProfil.blade.php
+++ resources/views/pageProfil.blade.php
@@ -5,27 +5,31 @@
 <div class="container">
 	<h1>Vos personnages</h1>
 	@foreach($characters as $character)
-	<div class="columns">
-  <div class="column">
+	<div class="columns ListePerso">
+  <div class="column PersoUnique">
     <img src='storage/{{$character->name}}.png'>
     {{$character->name}}
     <form action="{{route('delete_character', ['id' => $character->id])}}" method="POST">
                
                @csrf
-               <button class="btn btn-primary buttonBleu" type="submit" onclick="myFunction()">Supprimer ce personnage</button>               
+               <div class="place">
+               <button class="btn btn-primary buttonBleu" style="margin:2px;" type="submit" onclick="myFunction()">Supprimer ce personnage</button>               
             </form>
 
             <form action="{{route('use_character', ['id' => $character->id])}}" method="POST">
                
                @csrf
-               <button class="btn btn-primary buttonBleu" type="submit" onclick="myFunction1()">Utiliser ce personnage comme avatar</button>               
+               <button class="btn btn-primary buttonBleu" style="margin:2px;" type="submit" onclick="myFunction1()">Utiliser ce personnage comme avatar</button>   
+               </div>            
             </form>
   </div>
+
 @endforeach
 </div>
 
 </body>
 <script>
+
          function myFunction() {
        if(!confirm("Voulez-vous vraiment supprimer ce personnage?"))
        event.preventDefault();
