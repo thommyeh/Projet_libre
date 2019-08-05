d4d3cb77585dee7e7087d5f11397d9bf160e2819
Artorius Abraxas <jerome.griebel@epitech.eu>
2019-08-05 09:43:03 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- database/migrations/2018_08_10_034632_create_events_table.php
+++ database/migrations/2018_08_10_034632_create_events_table.php
@@ -16,18 +16,23 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+<<<<<<< Updated upstream
             $table->string('event_title',150);
             $table->string('event_start_date',15)->nullable();
             $table->string('event_start_time',15)->nullable();
             $table->string('event_end_date',15)->nullable();
             $table->string('event_end_time',15)->nullable();
             $table->text('event_description')->default('aucune description');
+=======
+            $table->string('event_title', 150);
+            $table->string('event_start_date', 15)->nullable();
+            $table->string('event_start_time', 15)->nullable();
+            $table->string('event_end_date', 15)->nullable();
+            $table->string('event_end_time', 15)->nullable();
+            $table->text('event_description')->nullable();
+>>>>>>> Stashed changes
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-      
-            
-            
-            
         });
     }
 
