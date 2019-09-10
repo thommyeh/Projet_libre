f4e7440c9561796fd6852d0144ce75393ea1f11c
Unknown <guillaume.frech@gmail.com>
2019-09-10 15:59:43 +0200

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,61 +108,20 @@
       </div>
    </nav>
    <body id="mypage">
+
       @show
       <main>
          @yield('content')
       </main>
+
    </body>
-   <!-- Footer -->
-<footer class="page-footer font-small special-color-dark pt-4">
-
-  <!-- Footer Elements -->
-  <div class="container">
-
-    <!-- Social buttons -->
-    <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-          <i class="fab fa-facebook-f"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-          <i class="fab fa-twitter"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-          <i class="fab fa-google-plus-g"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
-          <i class="fab fa-linkedin-in"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-dribbble mx-1">
-          <i class="fab fa-dribbble"> </i>
-        </a>
-      </li>
-    </ul>
-    <!-- Social buttons -->
-
-  </div>
-  <!-- Footer Elements -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+<footer id="sticky-footer" class="py-4 bg-dark text-white-50 footer footerC">
+  <small>© 2019 Copyright:
     <a href="{{ route('legals') }}"> My Help Mate</a>
-  </div>
-  <div class="footer-copyright text-center py-3">
-    <a href="{{ route('RGPD') }}"> Politique de confidentialité</a>
-  </div>
-  <!-- Copyright -->
+ <a href="{{ route('RGPD') }}"> Politique de confidentialité</a>
+    </small>
+  </footer>
 
-</footer>
-<!-- Footer -->
    <script type="text/javascript">
       function applyTheme (theme) {
        "use strict"
