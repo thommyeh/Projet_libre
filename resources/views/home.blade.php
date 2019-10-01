4ee00428b637af1b62d0e44da1c2914ad6c70ea0
Etsu <guillaume.frech@gmail.com>
2019-10-01 14:13:07 +0200

    IMPORTANT CHANGEMENT

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@ Une fois téléchargé, votre compagnon serra ensuite présent sur votre navigat
 
                   <footer class="footer fixed-bottom">
   <small>© 2019 Copyright:
-    <a href="{{ route('legals') }}"> My Help Mate</a>
+    <a href="{{ route('legals') }}"> My Help Mate </a> |
  <a href="{{ route('RGPD') }}"> Politique de confidentialité</a>
     </small>
   </footer>
